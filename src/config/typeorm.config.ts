a007bda7a9e5dc7d466dee7b18c0ee12976d7d03
shadat <shadat.rahman.464@gmail.com>
2023-04-20 23:07:26 +0600

    back to localhost

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,13 +2,13 @@ import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
 	type: 'postgres',
-	host: 'customerinfoshadat.postgres.database.azure.com',
+	host: 'localhost',
 	port: 5432,
-	username: 'shadat',
-	password: 'Madam007$',
-	database: 'postgres',
+	username: 'postgres',
+	password: 'root',
+	database: 'customerinfo',
 	entities: [__dirname + '/../**/*.entity.{js,ts}'],
 	autoLoadEntities: true,
 	synchronize: true,
-	ssl: true,
+	// ssl: true,
 }
