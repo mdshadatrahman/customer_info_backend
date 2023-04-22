c40ef18ec1a63cc7534fd16404aec2486dfce70d
shadat <shadat.rahman.464@gmail.com>
2023-04-22 11:41:33 +0600

    database moved to mysql

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,14 +1,19 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-	type: 'postgres',
+	// type: 'postgres',
+	// host: 'localhost',
+	// port: 5432,
+	// username: 'postgres',
+	// password: 'root',
+	// database: 'customerinfo',
+	type: 'mysql',
 	host: 'localhost',
-	port: 5432,
-	username: 'postgres',
+	port: 3306,
+	username: 'shadat',
 	password: 'root',
 	database: 'customerinfo',
 	entities: [__dirname + '/../**/*.entity.{js,ts}'],
 	autoLoadEntities: true,
 	synchronize: true,
-	// ssl: true,
 }
