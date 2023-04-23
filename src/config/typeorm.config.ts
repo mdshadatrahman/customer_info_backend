55e3be84640c2df366294b5919ad3e8ca9a19089
shadat <shadat.rahman.464@gmail.com>
2023-04-23 20:58:01 +0600

    mone nei

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,19 +1,20 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-	// type: 'postgres',
-	// host: 'localhost',
-	// port: 5432,
-	// username: 'postgres',
-	// password: 'root',
-	// database: 'customerinfo',
 	type: 'mysql',
 	host: 'localhost',
 	port: 3306,
 	username: 'shadat',
 	password: 'root',
 	database: 'customerinfo',
+	// type: 'mysql',
+	// host: 'localhost',
+	// port: 3306,
+	// username: 'shadatxy_shadat',
+	// password: 'gmjmnh#Y^vCA',
+	// database: 'shadatxy_customerinfo',
 	entities: [__dirname + '/../**/*.entity.{js,ts}'],
 	autoLoadEntities: true,
-	synchronize: true,
+
+	// synchronize: true,
 }
